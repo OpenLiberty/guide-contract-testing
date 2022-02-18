f601278d97a02da648859f02effdcc3638f745e7
Gilbert Kwan <gkwan@ca.ibm.com>
2022-02-18 14:49:03 -0500

    update versions

--- start/inventory/src/main/java/io/openliberty/guides/inventory/Inventory.java
+++ start/inventory/src/main/java/io/openliberty/guides/inventory/Inventory.java
@@ -1,6 +1,6 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2021 IBM Corporation and others.
+ * Copyright (c) 2021, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -12,9 +12,9 @@
 // end::copyright[]
 package io.openliberty.guides.inventory;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.Response;
 
 public class Inventory {
 
